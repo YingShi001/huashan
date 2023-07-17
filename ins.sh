2831d645a8efe2cadfb87fcb686ed59b282c0165
YingShi001 <135189461+YingShi001@users.noreply.github.com>
2023-07-17 11:53:18 +0800

    Add files via upload

---
+++ ins.sh
@@ -0,0 +1,71 @@
+!/bin/bash
+echo '##############update##############' &&
+sudo apt-get update &&
+echo '##############build-essential##############' &&
+sudo apt-get install -y build-essential &&
+echo '##############ninja-build##############' &&
+sudo apt-get install -y ninja-build &&
+echo '##############automake##############' &&
+sudo apt-get install -y automake &&
+echo '##############autoconf##############' &&
+sudo apt-get install -y autoconf &&
+echo '##############libtool##############' &&
+sudo apt-get install -y libtool &&
+echo '##############wget##############' &&
+sudo apt-get install -y wget &&
+echo '##############curl##############' &&
+sudo apt-get install -y curl &&
+echo '##############git##############' &&
+sudo apt-get install -y git &&
+echo '##############gcc##############' &&
+sudo apt-get install -y gcc &&
+echo '##############libssl-dev##############' &&
+sudo apt-get install -y libssl-dev &&
+echo '##############bc##############' &&
+sudo apt-get install -y bc &&
+echo '##############slib##############' &&
+sudo apt-get install -y slib &&
+echo '##############squashfs-tools##############' &&
+sudo apt-get install -y squashfs-tools &&
+echo '##############android-sdk-libsparse-utils##############' &&
+sudo apt-get install -y android-sdk-libsparse-utils &&
+echo '##############android-sdk-ext4-utils##############' &&
+sudo apt-get install -y android-sdk-ext4-utils &&
+echo '############## jq##############' &&
+sudo apt-get install -y jq &&
+echo '##############cmake##############' &&
+sudo apt-get install -y cmake &&
+echo '##############python3-distutils##############' &&
+sudo apt-get install -y python3-distutils &&
+echo '##############tclsh##############' &&
+sudo apt-get install -y tclsh &&
+echo '##############scons##############' &&
+sudo apt-get install -y scons &&
+echo '##############parallel##############' &&
+sudo apt-get install -y parallel &&
+echo '##############ssh-client##############' &&
+sudo apt-get install -y ssh-client &&
+echo '##############tree##############' &&
+sudo apt-get install -y tree &&
+echo '##############python3-dev##############' &&
+sudo apt-get install -y python3-dev &&
+echo '##############python3-pip##############' &&
+sudo apt-get install -y python3-pip &&
+echo '##############device-tree-compiler##############' &&
+sudo apt-get install -y device-tree-compiler &&
+echo '##############libssl-dev##############' &&
+sudo apt-get install -y libssl-dev &&
+echo '##############ssh##############' &&
+sudo apt-get install -y ssh &&
+echo '##############cpio##############' &&
+sudo apt-get install -y cpio &&
+echo '##############squashfs-tools##############' &&
+sudo apt-get install -y squashfs-tools &&
+echo '##############fakeroot##############' &&
+sudo apt-get install -y fakeroot &&
+echo '##############libncurses5##############' &&
+sudo apt-get install -y libncurses5 &&
+echo '##############flex##############' &&
+sudo apt-get install -y flex &&
+echo '##############bison##############' &&
+sudo apt-get install -y bison
